e027d7e83f76b6227aad83dbe632de13ed44a42b
j-goodman <johncurtisgoodman@gmail.com>
2026-05-14 17:30:58 -0400

    Fix double slash in url.

--- script.js
+++ script.js
@@ -1,6 +1,6 @@
 const form = document.getElementById("new-post-form")
 // const baseURL = `http://localhost:3000`
-const baseURL = `https://megaphone-server.onrender.com/`
+const baseURL = `https://megaphone-server.onrender.com`
 
 const getPosts = async () => {
     const response = await fetch(`${baseURL}/posts`)
